89f9a5ac0f90c102826a847795dc9e340fd6e33c
OtakuFlix <157716426+OtakuFlix@users.noreply.github.com>
2025-06-09 10:13:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,5 +23,5 @@ async def health_check():
     return JSONResponse(content={"status": "healthy"}, status_code=200)
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 8000))
+    port = int(os.getenv("PORT", 8080))
     uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
